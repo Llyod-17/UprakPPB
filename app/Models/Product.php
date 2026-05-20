18bee9bb6298445f01f9cd636870286cb034d07c
dylan a.c. <dacoaw@gmail.com>
2026-05-20 21:05:01 +0700

    dilang

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['category_id', 'name', 'slug', 'description', 'price', 'stock', 'is_active'];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'is_active' => 'boolean',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
